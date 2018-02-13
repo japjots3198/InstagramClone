1dfd53a68cbb97f32755ad956b18f5bc978c36f6
Japjot Singh <japjotsingh@japjots-macbook-air.local>
2018-02-14 01:51:05 +0530

    Initial Commit

---
+++ moments1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  moments1
+//
+//  Created by Japjot Singh on 14/02/18.
+//  Copyright © 2018 Japjot Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
